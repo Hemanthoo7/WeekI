8f9c7b37d3c92a76eeeb973160b8bc47e07e10b6
Hemanth Kumar <90673550+Hemanthoo7@users.noreply.github.com>
2022-08-17 18:50:56 +0530

    Add files via upload

---
+++ Payments/CustomPayment/Model/PaymentMethod.php
@@ -0,0 +1,14 @@
+<?php
+namespace Payments\CustomPayment\Model;
+/**
+ * Pay In Store payment method model
+ */
+class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
+{
+    /**
+     * Payment code
+     *
+     * @var string
+     */
+    protected $_code = 'custompayment';
+}
